47ace6604f748b76cd555daa929b0ce0ea8640f3
nathanoldridge <surpasstutors@gmail.com>
2023-06-06 17:05:33 -0400

    added homepage route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-''' READING THE DATA FILE 
 import os
+
+''' READING THE DATA FILE 
 # Current directory for Flask app
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Current directory for Flask app + file name
@@ -7,10 +8,9 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(APP_ROOT, 'data.csv')
 print("Accessing: ", file_path)
 print("never actuall read it.")
-
-
-
 '''
+
+PORT = os.environ["PORT"]
 print("App has started.")
 #import yfinance
 from flask import Flask
@@ -25,7 +25,10 @@ def homepage():
    return '<html><head><title>title</title></head><body>Home page strat.run</body></html>'
  
 if __name__ == '__main__':
-   app.run()
-
+    app.run(host='0.0.0.0', port=PORT) # use_reloader=True,  USE THIS FOR PROD
+    #app.run(host='127.0.0.1') # USE THIS FOR DEV
+    
+    
+    
 #print("App now shutting down.")
 
