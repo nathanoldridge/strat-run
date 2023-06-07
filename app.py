1f4676042a5d3bede6efd9f21f90b49527c1abdb
nathanoldridge <surpasstutors@gmail.com>
2023-06-07 16:09:24 -0400

    Added menu bar and About page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 __author__ = "Nathan Oldridge aka ChemistNate"
-__version__ = 0.1
+__version__ = 0.2
 __description__ = "A replacement for RunStrat.com"
 __website__ = "www.strat.run"
 
@@ -15,14 +15,14 @@ print("File Path String: ", file_path)
 print("Is that right?")
 
 
-PORT = os.environ["PORT"]
+#PORT = os.environ["PORT"]
 print("App has started.")
 #import yfinance
 from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/ticker/<tickerSymbol>')
-def hello_name(tickerSymbol):
+def singleTicker(tickerSymbol):
     tickerSymbol = str(tickerSymbol).upper()
     import yfinance as yf
     import getStrats
@@ -32,13 +32,17 @@ def hello_name(tickerSymbol):
     del hist['Stock Splits']
     return render_template('singleTicker.html', tickerX=tickerSymbol, dailyX=getStrats.getDailyStrats(hist), weeklyX=getStrats.getWeeklyStrats(hist))
 
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
 @app.route('/')
 def homepage():
-   return '<html><head><title>title</title></head><body>Home page strat.run</body></html>'
+   return render_template('index.html')
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT) # use_reloader=True,  USE THIS FOR PROD
-    #app.run(host='127.0.0.1') # USE THIS FOR DEV
+    #app.run(host='0.0.0.0', port=PORT) # use_reloader=True,  USE THIS FOR PROD
+    app.run(host='127.0.0.1') # USE THIS FOR DEV
     
     
     
