96a37a40b1338b13fed733fad8fd7496391c7073
nathanoldridge <surpasstutors@gmail.com>
2023-06-07 15:37:26 -0400

    Added template for single ticker and some Bootstrap CSS

--- app.py
+++ app.py
@@ -15,29 +15,30 @@ print("File Path String: ", file_path)
 print("Is that right?")
 
 
-PORT = os.environ["PORT"]
+#PORT = os.environ["PORT"]
 print("App has started.")
 #import yfinance
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/ticker/<tickerSymbol>')
 def hello_name(tickerSymbol):
+    tickerSymbol = str(tickerSymbol).upper()
     import yfinance as yf
     import getStrats
-    hist = yf.Ticker(str(tickerSymbol).upper()).history(period="2y", interval="1d")
+    hist = yf.Ticker(tickerSymbol).history(period="2y", interval="1d")
     del hist['Volume']
     del hist['Dividends']
     del hist['Stock Splits']
-    return '<html><body>You have asked for ' + tickerSymbol + '<br />DAILY:' + getStrats.getDailyStrats(hist) + '<br />WEEKLY:' + getStrats.getWeeklyStrats(hist) + '</body></html>'
+    return render_template('singleTicker.html', tickerX=tickerSymbol, dailyX=getStrats.getDailyStrats(hist), weeklyX=getStrats.getWeeklyStrats(hist))
 
 @app.route('/')
 def homepage():
    return '<html><head><title>title</title></head><body>Home page strat.run</body></html>'
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT) # use_reloader=True,  USE THIS FOR PROD
-    #app.run(host='127.0.0.1') # USE THIS FOR DEV
+    #app.run(host='0.0.0.0', port=PORT) # use_reloader=True,  USE THIS FOR PROD
+    app.run(host='127.0.0.1') # USE THIS FOR DEV
     
     
     
